8e6a3c6240840f9fff439b7fce7e4a92a4c00c4e
Praveen Pullaperuma <praveen98c@gmail.com>
2025-04-15 19:28:21 -0400

    Add paged return of restaurants

--- Backend/src/main/java/com/home/restaurant/service/RestaurantServiceImpl.java
+++ Backend/src/main/java/com/home/restaurant/service/RestaurantServiceImpl.java
@@ -1,5 +1,7 @@
 package com.home.restaurant.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.home.restaurant.dto.model.RestaurantDTO;
@@ -11,13 +13,20 @@ import com.home.restaurant.service.interfaces.RestaurantService;
 public class RestaurantServiceImpl implements RestaurantService {
 
 	private RestaurantRepository restaurantRepository;
-	
+
 	RestaurantServiceImpl(RestaurantRepository restaurantRepository) {
 		this.restaurantRepository = restaurantRepository;
 	}
-	
+
 	public RestaurantDTO getRestaurantById(Long id) {
 		Restaurant restaurant = restaurantRepository.getReferenceById(id);
-		return new RestaurantDTO(restaurant.getId(), restaurant.getName(), restaurant.getLocation(), restaurant.getImageUrl());
+		return new RestaurantDTO(restaurant.getId(), restaurant.getName(), restaurant.getLocation(),
+				restaurant.getImageUrl());
+	}
+
+	@Override
+	public Page<RestaurantDTO> getRestaurants(PageRequest pageable) {
+		return restaurantRepository.findAll(pageable)
+				.map((r -> new RestaurantDTO(r.getId(), r.getName(), r.getLocation(), r.getImageUrl())));
 	}
 }
